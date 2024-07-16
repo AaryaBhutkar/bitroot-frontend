149e592dd96fd1af4b1a787a5d6ba60f553ca50f
Chaitanya Paranjpe <112272151+cparanjpe@users.noreply.github.com>
2024-07-16 15:32:23 +0530

    Update ProfileInfo.jsx

--- src/components/admin/adminPages/ProfileInfo.jsx
+++ src/components/admin/adminPages/ProfileInfo.jsx
@@ -122,6 +122,8 @@ const ProfileInfo = ({ onClose }) => {
           name={name}
           value={profile[name]}
           onChange={handleInputChange}
+          min="0"
+          max="50"
           className="w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
         />
       ) : (
